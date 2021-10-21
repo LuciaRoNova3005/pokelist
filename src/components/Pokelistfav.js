c3ee6066d742180741d0c5ee7e1ca2b07f9f558f
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-10-21 13:01:53 +0200

    fixed pokemonlistfav

--- src/components/Pokelistfav.js
+++ src/components/Pokelistfav.js
@@ -2,8 +2,7 @@ import React from "react";
 import PokemonFav from "./PokemonFav";
 const Pokelistfav = (props) => {
   return (
-    <div className="container">
-      <h2>Pokemon Favoritos</h2>
+    <div>
       {props.dataFav.map((element) => (
         <PokemonFav
           key={element.id}
--- src/components/PokemonFav.js
+++ src/components/PokemonFav.js
@@ -1,20 +1,25 @@
 function PokemonFav(props) {
   return (
-    <ul className="card" key={props.key} id={props.id}>
-      <li>
-        <img className="image" src={props.img} alt={props.name}></img>
-      </li>
-
-      <li className="tittle">{props.name}</li>
-
-      <ul className="types">
-        {props.types.map((element, index) => (
-          <li className="text" key={index}>
-            {element}
+    <div>
+      <h2 className="tittle tittle2">Pokemon Favoritos</h2>
+      <div className="container">
+        <ul className="card" key={props.key} id={props.id}>
+          <li>
+            <img className="image" src={props.img} alt={props.name}></img>
           </li>
-        ))}
-      </ul>
-    </ul>
+
+          <li className="tittle">{props.name}</li>
+
+          <ul className="types">
+            {props.types.map((element, index) => (
+              <li className="text" key={index}>
+                {element}
+              </li>
+            ))}
+          </ul>
+        </ul>
+      </div>
+    </div>
   );
 }
 
